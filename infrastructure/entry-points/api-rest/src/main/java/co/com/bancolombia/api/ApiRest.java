2958f0af4e2dd968bf9a79b62b1bb815a314d74c
Ruben D Rodriguez Q <proyecto.rubendariorq@gmail.com>
2024-09-29 23:58:29 -0500

    Agrega un entrypoint persona

--- infrastructure/entry-points/api-rest/src/main/java/co/com/bancolombia/api/ApiRest.java
+++ infrastructure/entry-points/api-rest/src/main/java/co/com/bancolombia/api/ApiRest.java
@@ -1,20 +1,24 @@
 package co.com.bancolombia.api;
+import co.com.bancolombia.model.person.Person;
+import co.com.bancolombia.usecase.person.PersonUseCase;
 import lombok.AllArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping(value = "/api", produces = MediaType.APPLICATION_JSON_VALUE)
 @AllArgsConstructor
 public class ApiRest {
-//    private final MyUseCase useCase;
+    private final PersonUseCase personUseCase;
 
 
-    @GetMapping(path = "/path")
-    public String commandName() {
-//      return useCase.doAction();
-        return "Hello World";
+    @GetMapping(path = "/person/{id}")
+    public Optional<Person> getPerson(@PathVariable String id) {
+        return personUseCase.getPerson(id);
     }
 }
